7060b2f20c6440ad90e83f724cfffd1a6ef3b49e
oceansalle <oceanesalleiae@gmail.com>
2022-05-03 13:49:32 +0200

    developers component

--- tailwindcss/src/App.js
+++ tailwindcss/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'; 
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import About from './components/About';
+import AboutCard from './components/AboutCard';
+import Developers from './components/Developers';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      
+      <About />
+      <AboutCard />
+      <Developers />
       
     </div>
   );
